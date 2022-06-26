9e954fd0791e265f5cfcf7f868ca4daf4fd89e09
tariqHajeer <tareqhajer@gmail.com>
2022-06-26 20:03:18 +0300

    add comment

--- demo/Astar.py
+++ demo/Astar.py
@@ -1,6 +1,7 @@
 import heapq
 
 
+# city node for store city name and there distance
 class ctNode:
     def __init__(self, city, distance):
         self.city = str(city)
@@ -12,16 +13,21 @@ class map:
         self.rmap = {}
 
     def mkdir(self, city1, city2, distance):
+        # check if distance greater than zero
         if(int(distance) > 0):
+            # add city as key of object  and push a the other city and the distance from city1 to city2
             self.rmap.setdefault(city1, []).append(ctNode(city2, distance))
 
-#FIFO
+
+# a queue to store city depend on there cost
 class priorityQueue:
     def __init__(self):
         self.cities = []
+    # sotre city dened on there cost
 
     def push(self, city, cost):
         heapq.heappush(self.cities, (cost, city))
+    # get name of first city depend on there cost
 
     def pop(self):
         return heapq.heappop(self.cities)[1]
@@ -34,36 +40,48 @@ class priorityQueue:
 
 
 def astar(start, end, map: map):
+    # create an object to stor path
     path = {}
+    # create an object to store the distance of each city
     distance = {}
+
     q = priorityQueue()
     q.push(start, 0)
     distance[start] = 0
     path[start] = None
-
     while (q.isEmpty() == False):
         current = q.pop()
-
+        # if current path equal for end path
         if (current == end):
-            break    
+            break
+        # if city don't let me access to other cities
+        # skip this case
         if(current not in map.rmap):
             continue
+        # check fore each city that the current city let me access to it
         for new in map.rmap[current]:
+            # get cost for this city and added to privouse costs
             g_cost = distance[current] + int(new.distance)
-
+            # if i don't visit this city before or the total cost less thant the cost of this city in the prviouse visit
             if (new.city not in distance or g_cost < distance[new.city]):
+                # add the cost to distance by key named by city name
                 distance[new.city] = g_cost
-                f_cost = g_cost
-                q.push(new.city, f_cost)
+                # add for queue city and the final distance
+                q.push(new.city, g_cost)
+                # set the path of the new city is the current city
                 path[new.city] = current
+                # if the final path sould be like this A=>B=>Z
+                # then it well be sotre like this {"B":"A","Z","B"}
     return output(start, end, path, distance)
 
 
+#get the output 
 def output(start, end, path, distance):
+
+    if(end not in distance):
+        return {"path": ["not found"], "totalDistance": "not found"}
     finalpath = []
     i = end
-    if(end not in distance):
-        return {"path":["not found"],"totalDistance":"not found"}
     while (path.get(i) != None):
         finalpath.append(i)
         i = path[i]
--- demo/views.py
+++ demo/views.py
@@ -13,17 +13,22 @@ from django.views.decorators.csrf import csrf_exempt
 
 
 def home(request):
+    #if request was post 
     if(request.method == 'POST'):
+        #get data from reqeust 
         fromCity = request.POST["from"]
         toCity = request.POST["to"]
         countrymap = request.POST["map"]
         datafromJson = json.loads(countrymap)
+        #create object of map 
         m = map()
         for d in datafromJson:
+            #if city don't equeal to it self
             if(d["from"] != d["to"]):
+                #add data for map
                 m.mkdir(d["from"], d["to"], int(d["value"]))
-
+        #return the request of a star algorithm as json 
         return HttpResponse(json.dumps(astar(fromCity, toCity, m)))
-
+    #if not post then it well be get so return home html page 
     return render(request, 'home.html')
 
